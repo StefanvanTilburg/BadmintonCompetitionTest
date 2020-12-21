4dd86f0da58dc46af2e273e224a4d38136827dd2
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-12-21 12:28:06 +0100

    Init project

---
+++ src/test/java/nl/badminton/competition/badminton/competition/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.badminton.competition.badminton.competition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
